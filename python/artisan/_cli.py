0703fb60e25dad9a85a5767abec3741d88d53cbf
MasonMcGill <mmcgill@caltech.edu>
2019-04-02 16:00:30 -0700

    Removed CLI generation

--- python/artisan/_cli.py
+++
@@ -1,8 +0,0 @@
-__all__ = ['cli']
-
-#------------------------------------------------------------------------------
-# Command-line interface
-
-def cli() -> None:
-    'Run a command-line interface derived from the current scope.'
-    raise NotImplementedError()
