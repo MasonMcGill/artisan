9ecb706b80465852bb29578cea36dd279c4563b3
MasonMcGill <mmcgill@caltech.edu>
2019-06-03 20:14:09 -0700

    Added namespace tests

--- python/tests/test_namespaces.py
+++ python/tests/test_namespaces.py
@@ -1,7 +1,8 @@
-# WIP
+from types import SimpleNamespace
 
 from artisan._namespaces import Namespace, namespacify
 
+#------------------------------------------------------------------------------
 
 def test_namespaces():
     ns = Namespace(a=1, b=2)
@@ -19,4 +20,62 @@ def test_namespaces():
 
 
 def test_namespacify():
-    ...
+    obj = {
+        'bool': False,
+        'int': 1,
+        'float': 2.2,
+        'str': 'three',
+        'list': [0, 1, 2],
+        'dict': {'a': 0, 'b': 1},
+        'namespace': SimpleNamespace(c=2, d=3)
+    }
+    obj_as_ns = Namespace(
+        bool = False,
+        int = 1,
+        float = 2.2,
+        str = 'three',
+        list = [0, 1, 2],
+        dict = Namespace(a=0, b=1),
+        namespace = Namespace(c=2, d=3)
+    )
+    assert namespacify(obj) == obj_as_ns
+    assert isinstance(namespacify(obj).dict, Namespace)
+    assert isinstance(namespacify(obj).namespace, Namespace)
+
+
+def test_namespacify_with_nesting():
+    obj = {
+        'list': [
+            {'a': 0, 'b': 1},
+            SimpleNamespace(c=2, d=3)
+        ],
+        'dict': {
+            'a': {'a': 0, 'b': 1},
+            'b': SimpleNamespace(c=2, d=3)
+        },
+        'namespace': SimpleNamespace(
+            c = {'a': 0, 'b': 1},
+            d = SimpleNamespace(c=2, d=3)
+        )
+    }
+    obj_as_ns = Namespace(
+        list = [
+            Namespace(a=0, b=1),
+            Namespace(c=2, d=3)
+        ],
+        dict = Namespace(
+            a = Namespace(a=0, b=1),
+            b = Namespace(c=2, d=3)
+        ),
+        namespace = Namespace(
+            c = Namespace(a=0, b=1),
+            d = Namespace(c=2, d=3)
+        )
+    )
+    assert namespacify(obj) == obj_as_ns
+    assert isinstance(namespacify(obj).dict, Namespace)
+    assert isinstance(namespacify(obj).dict.a, Namespace)
+    assert isinstance(namespacify(obj).dict.b, Namespace)
+    assert isinstance(namespacify(obj).namespace, Namespace)
+    assert isinstance(namespacify(obj).namespace.c, Namespace)
+    assert isinstance(namespacify(obj).namespace.d, Namespace)
