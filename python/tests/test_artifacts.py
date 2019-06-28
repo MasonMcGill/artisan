4888d14049413e777f3b7e06b09665667c9dd0ec
MasonMcGill <mmcgill@caltech.edu>
2019-06-27 17:45:31 -0700

    Added and refactored artifact tests

--- python/tests/test_artifacts.py
+++ python/tests/test_artifacts.py
@@ -1,29 +1,86 @@
 from pathlib import Path
+from typing import List
 
 import h5py as h5
 import numpy as np
 
 from artisan._artifacts import Artifact
 
+#-- Helper functions ----------------------------------------------------------
+
+def data_file(path: Path) -> Path:
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_bytes(np.random.rand(3).tostring())
+    return path
+
+
+def data_file_concat(path: Path, args: List[Path]) -> Path:
+    path.parent.mkdir(parents=True, exist_ok=True)
+    path.write_bytes(b''.join(a.read_bytes() for a in args))
+    return path
+
+
+def assert_artifact_equals(artifact: Artifact, target: dict) -> None:
+    if '__type__' in target:
+        assert isinstance(artifact, target.pop('__type__'))
+    if '__path__' in target:
+        assert artifact.path == target.pop('__path__')
+    if '__conf__' in target:
+        assert artifact.conf == target.pop('__conf__')
+    if '__meta__' in target:
+        assert artifact.meta == target.pop('__meta__')
+
+    assert len(artifact) == len(target)
+    assert len(list(artifact)) == len(target)
+
+    for k, v in target.items():
+        assert k in artifact
+        assert k in list(artifact)
+        assert hasattr(artifact, k)
+
+        if isinstance(v, dict):
+            assert isinstance(artifact[k], Artifact)
+            assert isinstance(getattr(artifact, k), Artifact)
+            assert_artifact_equals(artifact[k], v)
+            assert_artifact_equals(getattr(artifact, k), v)
+
+        elif isinstance(v, Path):
+            k_mod = k.replace('.', '__')
+            assert (artifact.path / k).is_file()
+            assert isinstance(artifact[k], Path)
+            assert isinstance(getattr(artifact, k), Path)
+            assert artifact[k].read_bytes() == v.read_bytes()
+            assert getattr(artifact, k).read_bytes() == v.read_bytes()
+            assert getattr(artifact, k_mod).read_bytes() == v.read_bytes()
+
+        else:
+            assert (artifact.path / k).with_suffix('.h5').is_file()
+            assert isinstance(artifact[k], h5.Dataset)
+            assert isinstance(getattr(artifact, k), h5.Dataset)
+            assert np.array_equal(artifact[k][()], v)
+            assert np.array_equal(getattr(artifact, k)[()], v)
+            assert artifact[k].dtype == np.asarray(v).dtype
+            assert getattr(artifact, k).dtype == np.asarray(v).dtype
+
 #-- [Base class tests] Empty artifacts ----------------------------------------
 
 def test_empty_artifact_with_existing_dir(tmp_path: Path) -> None:
     a = Artifact(tmp_path)
-    assert a.path == tmp_path
-    assert a.conf == {}
-    assert a.meta == {'spec': None, 'status': 'done'}
-    assert len(a) == 0
-    assert list(a) == []
+    assert_artifact_equals(a, dict(
+        __path__ = tmp_path,
+        __conf__ = {},
+        __meta__ = {'spec': None, 'status': 'done'}
+    ))
     assert isinstance(a.nonexistent_entry, Artifact)
 
 
 def test_empty_artifact_with_nonexistent_dir(tmp_path: Path) -> None:
     a = Artifact(tmp_path / 'new_dir')
-    assert a.path == tmp_path / 'new_dir'
-    assert a.conf == {}
-    assert a.meta == {'spec': None, 'status': 'done'}
-    assert len(a) == 0
-    assert list(a) == []
+    assert_artifact_equals(a, dict(
+        __path__ = tmp_path / 'new_dir',
+        __conf__ = {},
+        __meta__ = {'spec': None, 'status': 'done'}
+    ))
     assert isinstance(a.nonexistent_entry, Artifact)
     assert not (tmp_path / 'new_dir').exists()
 
@@ -35,15 +92,7 @@ def test_float_assignment(tmp_path: Path) -> None:
     a.b = 2.5
     a['c'] = 3.5
     a['c'] = 4.5
-    assert len(a) == 2
-    assert 'b' in a
-    assert 'c' in a
-    assert (tmp_path / 'b.h5').is_file()
-    assert (tmp_path / 'c.h5').is_file()
-    assert isinstance(a.b, h5.Dataset)
-    assert isinstance(a.c, h5.Dataset)
-    assert np.array_equal(a.b, 2.5)
-    assert np.array_equal(a.c, 4.5)
+    assert_artifact_equals(a, {'b': 2.5, 'c': 4.5})
 
 
 def test_byte_string_assignment(tmp_path: Path) -> None:
@@ -52,15 +101,7 @@ def test_byte_string_assignment(tmp_path: Path) -> None:
     a.b = b'buzz'
     a['c'] = b'sea'
     a['c'] = b'ahoy!'
-    assert len(a) == 2
-    assert 'b' in a
-    assert 'c' in a
-    assert (tmp_path / 'b.h5').is_file()
-    assert (tmp_path / 'c.h5').is_file()
-    assert isinstance(a.b, h5.Dataset)
-    assert isinstance(a.c, h5.Dataset)
-    assert np.array_equal(a.b, b'buzz')
-    assert np.array_equal(a.c, b'ahoy!')
+    assert_artifact_equals(a, {'b': b'buzz', 'c': b'ahoy!'})
 
 
 def test_list_assignment(tmp_path: Path) -> None:
@@ -69,15 +110,10 @@ def test_list_assignment(tmp_path: Path) -> None:
     a.b = [4, 5, 6]
     a['c'] = [[7, 8], [9, 10]]
     a['c'] = [[11, 12, 13], [14, 15, 16]]
-    assert len(a) == 2
-    assert 'b' in a
-    assert 'c' in a
-    assert (tmp_path / 'b.h5').is_file()
-    assert (tmp_path / 'c.h5').is_file()
-    assert isinstance(a.b, h5.Dataset)
-    assert isinstance(a.c, h5.Dataset)
-    assert np.array_equal(a.b, [4, 5, 6])
-    assert np.array_equal(a.c, [[11, 12, 13], [14, 15, 16]])
+    assert_artifact_equals(a, {
+        'b': [4, 5, 6],
+        'c': [[11, 12, 13], [14, 15, 16]]
+    })
 
 
 def test_array_assignment(tmp_path: Path) -> None:
@@ -86,167 +122,182 @@ def test_array_assignment(tmp_path: Path) -> None:
     a.b = np.zeros((4, 5, 6), dtype='float32')
     a['c'] = np.ones((4, 5, 6), dtype='float32')
     a['c'] = np.ones((2, 3), dtype='uint16')
-    assert len(a) == 2
-    assert 'b' in a
-    assert 'c' in a
-    assert (tmp_path / 'b.h5').is_file()
-    assert (tmp_path / 'c.h5').is_file()
-    assert isinstance(a.b, h5.Dataset)
-    assert isinstance(a.c, h5.Dataset)
-    assert np.array_equal(a.b, np.zeros((4, 5, 6), dtype='float32'))
-    assert np.array_equal(a.c, np.ones((2, 3), dtype='uint16'))
-    assert a.b.dtype == 'float32'
-    assert a.c.dtype == 'uint16'
+    assert_artifact_equals(a, {
+        'b': np.zeros((4, 5, 6), dtype='float32'),
+        'c': np.ones((2, 3), dtype='uint16')
+    })
 
 
 def test_path_assignment(tmp_path: Path) -> None:
-    path_b0 = tmp_path / 'some_path.txt'
-    path_b1 = tmp_path / 'some_other_path.txt'
-    path_c0 = tmp_path / 'yet_another_path.txt'
-    path_c1 = tmp_path / 'gosh_so_many_paths.txt'
-    path_b0.write_text('[Some worthless nonsense]')
-    path_b1.write_text('I am bee text.')
-    path_c0.write_text('[The complete works of Shakespeare]')
-    path_c1.write_text('I am sea text.')
     a = Artifact(tmp_path / 'artifact')
-    a.b__txt = path_b0
-    a.b__txt = path_b1
-    a['c.txt'] = path_c0
-    a['c.txt'] = path_c1
-    assert len(a) == 2
-    assert 'b.txt' in a
-    assert 'c.txt' in a
-    assert (tmp_path / 'artifact/b.txt').is_file()
-    assert (tmp_path / 'artifact/c.txt').is_file()
-    assert isinstance(a.b__txt, Path)
-    assert isinstance(a.c__txt, Path)
-    assert a.b__txt.read_text() == 'I am bee text.'
-    assert a.c__txt.read_text() == 'I am sea text.'
+    a.b__bin = data_file(tmp_path / 'b0.bin')
+    a.b__bin = data_file(tmp_path / 'b1.bin')
+    a['c.bin'] = data_file(tmp_path / 'c0.bin')
+    a['c.bin'] = data_file(tmp_path / 'c1.bin')
+    assert_artifact_equals(a, {
+        'b.bin': tmp_path / 'b1.bin',
+        'c.bin': tmp_path / 'c1.bin'
+    })
 
 
 def test_dict_assignment(tmp_path: Path) -> None:
     a = Artifact(tmp_path)
     a.dict = dict(a=[1, 2, 3], b=dict(c=[4], d=[5, 6]))
-    assert (tmp_path / 'dict/a.h5').is_file()
-    assert (tmp_path / 'dict/b/c.h5').is_file()
-    assert (tmp_path / 'dict/b/d.h5').is_file()
-    assert 'dict' in a
-    assert 'a' in a.dict
-    assert 'b' in a.dict
-    assert 'c' in a.dict.b # type: ignore
-    assert 'd' in a.dict.b # type: ignore
-    assert 'c' not in a.dict
-    assert 'd' not in a.dict
-    assert isinstance(a.dict, Artifact)
-    assert isinstance(a.dict.a, h5.Dataset)
-    assert isinstance(a.dict.b, Artifact)
-    assert isinstance(a.dict.b.c, h5.Dataset)
-    assert isinstance(a.dict.b.d, h5.Dataset)
-    assert np.array_equal(a.dict.a, [1, 2, 3])
-    assert np.array_equal(a.dict.b.c, [4])
-    assert np.array_equal(a.dict.b.d, [5, 6])
+    assert_artifact_equals(a, {
+        'dict': {
+            'a': [1, 2, 3],
+            'b': {'c': [4], 'd': [5, 6]}
+        }
+    })
 
 
 def test_artifact_assignment(tmp_path: Path) -> None:
-    (tmp_path / 'text.txt').write_text('effigy')
     a_src = Artifact(tmp_path / 'a_src')
     a_src.b.c = b'bee sea'
     a_src.d.e = [1, 2, 3, 4]
-    a_src.f.g__txt = tmp_path / 'text.txt'
+    a_src.f.g__bin = data_file(tmp_path / 'effigy.bin')
     a_dst = Artifact(tmp_path / 'a_dst')
     a_dst.a = a_src
-    assert len(a_dst) == 1
-    assert len(a_dst.a) == 3
-    assert len(a_dst.a.b) == 1
-    assert len(a_dst.a.d) == 1
-    assert len(a_dst.a.f) == 1
-    assert (tmp_path / 'a_dst/a/b/c.h5').is_file()
-    assert (tmp_path / 'a_dst/a/d/e.h5').is_file()
-    assert (tmp_path / 'a_dst/a/f/g.txt').is_file()
-    assert 'b' in a_dst.a
-    assert 'c' in a_dst.a.b
-    assert 'd' in a_dst.a
-    assert 'e' in a_dst.a.d
-    assert 'f' in a_dst.a
-    assert 'g.txt' in a_dst.a.f
-    assert isinstance(a_dst.a.b, Artifact)
-    assert isinstance(a_dst.a.b.c, h5.Dataset)
-    assert isinstance(a_dst.a.d, Artifact)
-    assert isinstance(a_dst.a.d.e, h5.Dataset)
-    assert isinstance(a_dst.a.f, Artifact)
-    assert isinstance(a_dst.a.f.g__txt, Path)
-    assert np.array_equal(a_dst.a.b.c, b'bee sea')
-    assert np.array_equal(a_dst.a.d.e, [1, 2, 3, 4])
-    assert a_dst.a.f.g__txt.read_text() == 'effigy'
+    assert_artifact_equals(a_dst, {
+        'a': {
+            'b': {'c': b'bee sea'},
+            'd': {'e': [1, 2, 3, 4]},
+            'f': {'g.bin': tmp_path / 'effigy.bin'}
+        }
+    })
 
 #-- [Base class tests] Entry extension ----------------------------------------
 
-# def test_list_extension(tmp_path: Path) -> None:
-#     a = Artifact(tmp_path)
-#     a.b = [1, 2, 3]
-#     a.extend('b', [4, 5, 6])
-#     a.extend('c', [[7, 8], [9, 10]])
-#     a.extend('c', [[11, 12]])
-#     assert len(a) == 2
-#     assert 'b' in a
-#     assert 'c' in a
-#     assert (tmp_path / 'b.h5').is_file()
-#     assert (tmp_path / 'c.h5').is_file()
-#     assert isinstance(a.b, h5.Dataset)
-#     assert isinstance(a.c, h5.Dataset)
-#     assert np.array_equal(a.b, [1, 2, 3, 4, 5, 6])
-#     assert np.array_equal(a.c, [[7, 8], [9, 10], [11, 12]])
+def test_list_extension(tmp_path: Path) -> None:
+    a = Artifact(tmp_path)
+    a.extend('b', [[7, 8], [9, 10]])
+    a.extend('b', [[11, 12]])
+    assert_artifact_equals(a, {
+        'b': [[7, 8], [9, 10], [11, 12]]
+    })
 
 
-# def test_array_extension(tmp_path: Path) -> None:
-#     a = Artifact(tmp_path)
-#     a.b = np.float32([1, 2, 3])
-#     a.extend('b', np.float32([4, 5, 6]))
-#     a.extend('c', np.uint16([[7, 8], [9, 10]]))
-#     a.extend('c', np.uint16([[11, 12]]))
-#     assert len(a) == 2
-#     assert 'b' in a
-#     assert 'c' in a
-#     assert (tmp_path / 'b.h5').is_file()
-#     assert (tmp_path / 'c.h5').is_file()
-#     assert isinstance(a.b, h5.Dataset)
-#     assert isinstance(a.c, h5.Dataset)
-#     assert np.array_equal(a.b, [1, 2, 3, 4, 5, 6])
-#     assert np.array_equal(a.c, [[7, 8], [9, 10], [11, 12]])
-#     assert a.b.dtype == 'float32'
-#     assert a.c.dtype == 'uint16'
+def test_array_extension(tmp_path: Path) -> None:
+    a = Artifact(tmp_path)
+    a.extend('b', np.uint16([[7, 8], [9, 10]]))
+    a.extend('b', np.uint16([[11, 12]]))
+    assert_artifact_equals(a, {
+        'b': np.uint16([[7, 8], [9, 10], [11, 12]])
+    })
 
 
 def test_path_extension(tmp_path: Path) -> None:
-    ...
+    a = Artifact(tmp_path / 'a')
+    a.extend('b.bin', data_file(tmp_path / 'b0.bin'))
+    a.extend('b.bin', data_file(tmp_path / 'b1.bin'))
+    assert_artifact_equals(a, {
+        'b.bin': data_file_concat(tmp_path / 'b2.bin', [
+            tmp_path / 'b0.bin', tmp_path / 'b1.bin'
+        ])
+    })
 
 
 def test_dict_extension(tmp_path: Path) -> None:
-    ...
+    a = Artifact(tmp_path / 'a')
+    a.extend('b', {
+        'c': np.empty((0, 2), dtype='uint16'),
+        'd.bin': data_file(tmp_path / 'd0.bin'),
+        'e': {'f': [0.1, 0.2]}
+    })
+    a.extend('b', {
+        'c': np.uint16([[1, 2], [3, 4]]),
+        'd.bin': data_file(tmp_path / 'd1.bin'),
+        'e': {'f': [0.3, 0.4, 0.5]}
+    })
+    assert_artifact_equals(a, {
+        'b': {
+            'c': np.uint16([[1, 2], [3, 4]]),
+            'd.bin': data_file_concat(tmp_path / 'd2.bin', [
+                tmp_path / 'd0.bin', tmp_path / 'd1.bin']
+            ),
+            'e': {'f': [0.1, 0.2, 0.3, 0.4, 0.5]}
+        }
+    })
 
 
 def test_artifact_extension(tmp_path: Path) -> None:
-    ...
+    a0 = Artifact(tmp_path / 'a0')
+    a0.b = [b'hello']
+    a0.c__bin = data_file(tmp_path / 'c0.bin')
+    a0.d = {'e': [[0.0, 1.0]], 'f.bin': data_file(tmp_path / 'f0.bin')}
+
+    a1 = Artifact(tmp_path / 'a1')
+    a1.b = [b'good', b'bye']
+    a1.c__bin = data_file(tmp_path / 'c1.bin')
+    a1.d = {'e': np.empty((0, 2)), 'f.bin': data_file(tmp_path / 'f1.bin')}
+
+    a2 = Artifact(tmp_path / 'a2')
+    a2.extend('subartifact', a0)
+    a2.extend('subartifact', a1)
+
+    assert_artifact_equals(a2, {
+        'subartifact': {
+            'b': [b'hello', b'good', b'bye'],
+            'c.bin': data_file_concat(tmp_path / 'c2.bin', [
+                tmp_path / 'c0.bin', tmp_path / 'c1.bin']
+            ),
+            'd': {
+                'e': [[0.0, 1.0]],
+                'f.bin': data_file_concat(tmp_path / 'f2.bin', [
+                    tmp_path / 'f0.bin', tmp_path / 'f1.bin']
+                )
+            }
+        }
+    })
 
 #-- [Base class tests] Entry deletion -----------------------------------------
 
 def test_array_file_deletion(tmp_path: Path) -> None:
-    a = Artifact(tmp_path)
+    a = Artifact(tmp_path / 'a')
     a.b = [1, 2, 3]
     a.c = b'four five six'
+    a.d = [7, 8]
+    a.e = {'blue': b'jeans'}
+    a.f__bin = data_file(tmp_path / 'data.bin')
     del a.b
     del a['c']
-    assert len(a) == 0
-    assert 'b' not in a
-    assert 'c' not in a
+    assert_artifact_equals(a, {
+        'd': [7, 8],
+        'e': {'blue': b'jeans'},
+        'f.bin': tmp_path / 'data.bin'
+    })
 
 
 def test_opaque_file_deletion(tmp_path: Path) -> None:
-    ...
+    a = Artifact(tmp_path / 'a')
+    a.b__bin = data_file(tmp_path / 'b.bin')
+    a.c__bin = data_file(tmp_path / 'c.bin')
+    a.d = [7, 8]
+    a.e = {'blue': b'jeans'}
+    a.f__bin = data_file(tmp_path / 'data.bin')
+    del a.b__bin
+    del a['c.bin']
+    assert_artifact_equals(a, {
+        'd': [7, 8],
+        'e': {'blue': b'jeans'},
+        'f.bin': tmp_path / 'data.bin'
+    })
 
 
 def test_artifact_deletion(tmp_path: Path) -> None:
-    ...
+    a = Artifact(tmp_path / 'a')
+    a.b = {'aa': {'bb': 0, 'cc': 1}}
+    a.c = {'dd': {'ee': [2, 3, 4]}}
+    a.d = [7, 8]
+    a.e = {'blue': b'jeans'}
+    a.f__bin = data_file(tmp_path / 'data.bin')
+    del a.b
+    del a['c']
+    assert_artifact_equals(a, {
+        'd': [7, 8],
+        'e': {'blue': b'jeans'},
+        'f.bin': tmp_path / 'data.bin'
+    })
 
 #-- [Subclass tests] Construction ---------------------------------------------
 
