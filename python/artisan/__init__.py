b77fbda6714b0d9c5087061ee3a2f8036a8b27e4
MasonMcGill <mmcgill@caltech.edu>
2019-07-01 05:58:20 -0700

    Added compatibility definitions

--- python/artisan/__init__.py
+++ python/artisan/__init__.py
@@ -23,3 +23,24 @@ Artisan.__module__ = __name__
 #-- Wonky alias docstring definitions -----------------------------------------
 
 EncodedFile = EncodedFile; 'An alias for `pathlib.Path`' # type: ignore
+
+#-- Backwards compatibility definitions ---------------------------------------
+
+from contextlib import contextmanager
+
+def push_conf(**kwargs):
+    Artisan.push(**kwargs)
+
+def pop_conf():
+    return Artisan.pop()
+
+@contextmanager
+def using_conf(**kwargs):
+    push_conf(**kwargs); yield
+    pop_conf()
+
+def get_conf():
+    return Artisan.get_current()
+
+def serve():
+    Artisan.get_current().serve()
